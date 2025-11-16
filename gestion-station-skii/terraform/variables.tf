f471a0b42efd7b77cd22220ec69a6bdd08814de2
nesrine-khiari <147988492+nesrine-khiari@users.noreply.github.com>
2025-11-16 20:06:23 +0100

    Refactor Terraform variables for EKS setup
    Updated role ARN and added subnet CIDR variables.

--- gestion-station-skii/terraform/variables.tf
+++ gestion-station-skii/terraform/variables.tf
@@ -1,39 +1,35 @@
-
-
-
 variable "aws_region" {
   description = "La région AWS"
   type        = string
-  default     = "us-east-1"  # Région mise à jour
+  default     = "us-east-1"
 }
 
 variable "cluster_name" {
   description = "Nom du cluster EKS"
   type        = string
-  default     = "mykubernetes"  # Nom du cluster mis à jour
+  default     = "mykubernetes"
 }
-
-variable "subnet_ids" {
-  description = "IDs of the subnets for EKS"
-  type        = list(string)
-}
-
 
 variable "role_arn" {
   description = "ARN du rôle IAM pour EKS"
   type        = string
-  default     = "arn:aws:iam::744983671605:role/LabRole"  # Valeur par défaut
-}
-
-variable "vpc_id" {
-  description = "L'ID du VPC pour le cluster EKS"
-  type        = string
-  default     = "vpc-04c984733bc5b455e"  # Remplacez par votre ID de VPC réel
+  default     = "arn:aws:iam::775955735712:role/LabRole"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"  # Modifiez-le selon vos besoins
+  default     = "10.0.0.0/16"
 }
 
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
