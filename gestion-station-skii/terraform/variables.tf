ee7c35e4116b848234f9572289089ac2caa8e460
nesrine-khiari <147988492+nesrine-khiari@users.noreply.github.com>
2025-11-16 18:56:17 +0100

    Refactor variable definitions in variables.tf
    Updated variable descriptions and organization in variables.tf.

--- gestion-station-skii/terraform/variables.tf
+++ gestion-station-skii/terraform/variables.tf
@@ -1,33 +1,26 @@
+# ------------------------------
+# Région AWS
+# ------------------------------
 variable "aws_region" {
-  description = "La région AWS"
+  description = "La région AWS où se trouve le cluster et le VPC"
   type        = string
-  default     = "us-east-1"  # Région mise à jour
+  default     = "us-east-1"
 }
 
+# ------------------------------
+# Nom du cluster
+# ------------------------------
 variable "cluster_name" {
   description = "Nom du cluster EKS"
   type        = string
-  default     = "mykubernetes"  # Nom du cluster mis à jour
+  default     = "mykubernetes"
 }
 
-variable "subnet_ids" {
-  type    = list(string)
-  default = []
-}
-
-
-variable "role_arn" {
-  description = "ARN du rôle IAM pour EKS"
-  type        = string
-  default     = "arn:aws:iam::775955735712:role/LabRole"  # Valeur par défaut
-}
-
-
+# ------------------------------
+# CIDR du VPC
+# ------------------------------
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "CIDR block du VPC"
   type        = string
-  default     = "10.0.0.0/16"  # Modifiez-le selon vos besoins
+  default     = "10.0.0.0/16"
 }
-
-
-
