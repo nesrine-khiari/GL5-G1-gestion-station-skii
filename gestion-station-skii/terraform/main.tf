8ffe969210810b52ef97f6f9165aa3da8932778b
salim-brahem <salimbrahem18@gmail.com>
2025-11-18 15:25:59 +0100

    provider-main

--- gestion-station-skii/terraform/main.tf
+++ gestion-station-skii/terraform/main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = var.aws_region
+}
 
 # Data source pour les zones de disponibilité
 data "aws_availability_zones" "available" {
--- gestion-station-skii/terraform/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-
-  assume_role {
-    role_arn = var.role_arn
-  }
-}
