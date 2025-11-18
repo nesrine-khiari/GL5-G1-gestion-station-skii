f7e4dd72fb4d7fde70e67eedac2a10483e24eea1
salim-brahem <salimbrahem18@gmail.com>
2025-11-18 14:59:12 +0100

    provider

--- gestion-station-skii/terraform/providers.tf
+++ gestion-station-skii/terraform/providers.tf
@@ -1,14 +1,7 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.aws_region
+
+  assume_role {
+    role_arn = var.role_arn
+  }
 }
